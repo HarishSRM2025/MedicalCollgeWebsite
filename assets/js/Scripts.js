c6cf0f4f3ab9751c723fd0ae17fa470880fc30ee
HarishSRM2025 <harishsrm25@gmail.com>
2025-10-22 10:14:13 +0530

    anchor issue fix

--- assets/js/Scripts.js
+++ assets/js/Scripts.js
@@ -230,17 +230,5 @@
             });
         });
 
-        // Smooth scroll for navigation
-        document.querySelectorAll('nav a').forEach(anchor => {
-            anchor.addEventListener('click', function(e) {
-                e.preventDefault();
-                const target = document.querySelector(this.getAttribute('href'));
-                if (target) {
-                    target.scrollIntoView({
-                        behavior: 'smooth',
-                        block: 'start'
-                    });
-                }
-            });
-        });
+    
     
