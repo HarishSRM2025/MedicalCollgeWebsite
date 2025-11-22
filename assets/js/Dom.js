8cc79124485a837503a5db25a4efefbd0b760add
HarishSRM2025 <harishsrm25@gmail.com>
2025-11-22 11:10:33 +0530

    latest

--- assets/js/Dom.js
+++ assets/js/Dom.js
@@ -29,6 +29,12 @@
           </svg>
           Admission
         </a>
+        <a href="tel:+919876543210" class="quick-link">
+          <svg class="quick-link-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"/>
+          </svg>
+          Emergency
+        </a>
         <a href="./aboutus.html#milestones" class="quick-link">
           <svg class="quick-link-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
             <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
